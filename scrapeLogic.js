fab1f64b8ecd55edda514e422e116d2cbfdb96d9
rasadlli <resadesedli26@gmail.com>
2024-11-27 13:54:12 +0400

    some chnages

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -7,9 +7,12 @@ const scrapeLogic = async (res) => {
             "--disable-setuid-sandbox",
             "--no-sandbox",
             "--single-process",
-            "--no-zygote"
+            "--no-zygote",
         ],
-        executablePath: "/usr/bin/google-chrome-browser"
+        executablePath:
+            process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath(),
     });
     try {
         const page = await browser.newPage();

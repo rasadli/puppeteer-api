65afb2f2f246676fdca7b21f8941728d731b73ed
rasadlli <resadesedli26@gmail.com>
2024-11-27 11:52:29 +0400

    Updated args and exucatable path for Puppeteer

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const {scrapeLogic} = require("./scrapeLogic")
-const app = express()
+const express = require("express");
+const { scrapeLogic } = require("./scrapeLogic");
+const app = express();
 
-const PORT = process.env.PORT || 4000
-
-app.get("/", (req, res) => {
-    res.send("Render Puppeteer server is up and running!")
-})
+const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-    scrapeLogic(res)
-})
+  scrapeLogic(res);
+});
+
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
 app.listen(PORT, () => {
-    console.log(`Listeting on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
